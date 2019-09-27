6f35d280092b687d0009baa8a3c56c7c95e71c63
SiamSami <33563064+SiamSami@users.noreply.github.com>
2019-09-27 20:41:19 +0600

    Update deepin_kwin_settings.cpp

--- deepin_kwin_settings.cpp
+++ deepin_kwin_settings.cpp
@@ -5,7 +5,7 @@
 #include <QListWidgetItem>
 #include <QMessageBox>
 using namespace std;
-QListWidgetItem* checkbox[7];
+QListWidgetItem* checkbox[10];
 string home = getenv("HOME");
 bool plugins = false;
 bool effectcover = false;
@@ -24,12 +24,16 @@ deepin_kwin_settings::deepin_kwin_settings(QWidget *parent) :
     checkbox[2]->setCheckState(Qt::Unchecked);
     checkbox[3] = new QListWidgetItem("Cover switch", ui->listWidget);
     checkbox[3]->setCheckState(Qt::Unchecked);
-    checkbox[4] = new QListWidgetItem("\"Fade\" animation when closing or opening programs", ui->listWidget);
+    checkbox[4] = new QListWidgetItem("Sliding popup animation", ui->listWidget);
     checkbox[4]->setCheckState(Qt::Unchecked);
-    checkbox[5] = new QListWidgetItem("Sliding popup animation", ui->listWidget);
+    checkbox[5] = new QListWidgetItem("Show Desktop while switching through programs", ui->listWidget);
     checkbox[5]->setCheckState(Qt::Unchecked);
-    checkbox[6] = new QListWidgetItem("Show \"Desktop\" while switching through programs", ui->listWidget);
+    checkbox[6] = new QListWidgetItem("Translucency", ui->listWidget);
     checkbox[6]->setCheckState(Qt::Unchecked);
+    checkbox[7] = new QListWidgetItem("Sheet: Make dialogues smoothly fly in and out", ui->listWidget);
+    checkbox[7]->setCheckState(Qt::Unchecked);
+    checkbox[8] = new QListWidgetItem("Glide windows as they appear or disappear", ui->listWidget);
+    checkbox[8]->setCheckState(Qt::Unchecked);
     ifstream file(home+"/.config/kwinrc");
     //QString str;
     std::string temp;
@@ -59,17 +63,22 @@ deepin_kwin_settings::deepin_kwin_settings(QWidget *parent) :
             checkbox[3]->setFlags(checkbox[3]->flags() | Qt::ItemIsUserCheckable);
             checkbox[3]->setCheckState(Qt::Checked);
         }
-        if(temp == "kwin4_effect_fadeEnabled[$d]"){
-            checkbox[4]->setFlags(checkbox[4]->flags() | Qt::ItemIsUserCheckable);
+        if(temp == "slidingpopupsEnabled[$d]"){
+            checkbox[4]->setFlags(checkbox[5]->flags() | Qt::ItemIsUserCheckable);
             checkbox[4]->setCheckState(Qt::Checked);
         }
-        if(temp == "slidingpopupsEnabled[$d]"){
-            checkbox[5]->setFlags(checkbox[5]->flags() | Qt::ItemIsUserCheckable);
+        if(temp == "ShowDesktopMode=1"){
+            checkbox[5]->setFlags(checkbox[6]->flags() | Qt::ItemIsUserCheckable);
             checkbox[5]->setCheckState(Qt::Checked);
         }
-        if(temp == "ShowDesktopMode=1"){
-            checkbox[6]->setFlags(checkbox[6]->flags() | Qt::ItemIsUserCheckable);
+        if(temp == "kwin4_effect_translucencyEnabled[$d]"){
             checkbox[6]->setCheckState(Qt::Checked);
+        }
+        if(temp == "sheetEnabled=true"){
+            checkbox[7]->setCheckState(Qt::Checked);
+        }
+        if(temp == "glideEnabled=true"){
+            checkbox[8]->setCheckState(Qt::Checked);
         }
     }
     file.close();
@@ -110,16 +119,27 @@ void deepin_kwin_settings::on_pushButton_clicked()
         }else {
             backup.replace("coverswitchEnabled=true", "coverswitchEnabled=false");
         }
-        if(checkbox[4]->checkState()){
-            backup.replace("kwin4_effect_fadeEnabled=false","kwin4_effect_fadeEnabled[$d]");
+        if(checkbox[6]->checkState()){
+            backup.replace("kwin4_effect_translucencyEnabled=false", "kwin4_effect_translucencyEnabled[$d]");
         }else {
-            backup.replace("kwin4_effect_fadeEnabled[$d]","kwin4_effect_fadeEnabled=false");
+            backup.replace("kwin4_effect_translucencyEnabled[$d]", "kwin4_effect_translucencyEnabled=false");
         }
-        if(checkbox[5]->checkState()){
+        if(checkbox[4]->checkState()){
             backup.replace("slidingpopupsEnabled=false", "slidingpopupsEnabled[$d]");
         }else{
             backup.replace("slidingpopupsEnabled[$d]", "slidingpopupsEnabled=false");
         }
+        if(checkbox[7]->checkState()){
+            backup.replace("sheetEnabled=false", "sheetEnabled=true");
+        }else{
+            backup.replace("sheetEnabled=true", "sheetEnabled=false");
+        }
+        if(checkbox[8]->checkState()){
+            backup.replace("glideEnabled=false", "glideEnabled=true");
+        }else{
+            backup.replace("glideEnabled=true","glideEnabled=false");
+        }
+
     }else {
         if(!effectcover){
             backup.append("[Effect-CoverSwitch]\nReflection=true\nTabBox=true\nTabBoxAlternative=true\n");
@@ -146,18 +166,28 @@ void deepin_kwin_settings::on_pushButton_clicked()
             backup.append("coverswitchEnabled=false\n");
         }
         if(checkbox[4]->checkState()){
-            backup.append("kwin4_effect_fadeEnabled[$d]\n");
-        }else {
-            backup.append("kwin4_effect_fadeEnabled=false\n");
-        }
-        if(checkbox[5]->checkState()){
             backup.append("slidingpopupsEnabled[$d]\n");
         }else{
             backup.append("slidingpopupsEnabled=false\n");
         }
+        if(checkbox[6]->checkState()){
+            backup.append("kwin4_effect_translucencyEnabled[$d]");
+        }else {
+            backup.append("kwin4_effect_translucencyEnabled=false");
+        }
+        if(checkbox[7]->checkState()){
+            backup.append("sheetEnabled=true");
+        }else{
+            backup.append("sheetEnabled=false");
+        }
+        if(checkbox[8]->checkState()){
+            backup.append("glideEnabled=true");
+        }else{
+            backup.append("glideEnabled=false");
+        }
     }
     if(tabBox){
-        if(checkbox[6]->checkState()){
+        if(checkbox[5]->checkState()){
             backup.replace("ShowDesktopMode=0","ShowDesktopMode=1");
         }else {
             backup.replace("ShowDesktopMode=1","ShowDesktopMode=0");
